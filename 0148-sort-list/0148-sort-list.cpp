8373c09b3782ae58ccc40ce2ee73e03a88db0d5e
Kiran Dhawan <134703701+Key-runnnnnnn@users.noreply.github.com>
2026-07-26 16:56:19 +0530

    Time: 18 ms (55.56%), Space: 58.3 MB (53.9%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,20 +10,40 @@
  */
 class Solution {
 public:
+    ListNode* middle(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode *slow = head, *fast = head->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge2Lists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr)
+            return list2;
+        if (list2 == nullptr)
+            return list1;
+        if (list1->val <= list2->val) {
+            list1->next = merge2Lists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = merge2Lists(list2->next, list1);
+            return list2;
+        }
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int> v;
-        ListNode* temp = head;
-        while(temp){
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(v.begin(),v.end());
-        temp = head;
-        int i=0;
-        while(temp){
-            temp->val = v[i++];
-            temp=temp->next;
-        }
-        return head;
+        if (!head || !head->next)
+            return head;
+        ListNode* mid = middle(head);
+        ListNode* lefthead = head;
+        ListNode* righthead = mid->next;
+        mid->next = nullptr;
+        lefthead = sortList(lefthead);
+        righthead = sortList(righthead);
+        return merge2Lists(lefthead, righthead);
     }
 };
