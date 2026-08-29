1e25cf2ee2f730b19341a23a25cd1fdad177d05f
Kiran Dhawan <134703701+Key-runnnnnnn@users.noreply.github.com>
2026-08-29 15:23:35 +0530

    Time: 2 ms (35.93%), Space: 10.5 MB (71.52%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,21 @@
 class Solution {
 private:
-    void f(vector<int>& temp, vector<vector<int>>& ans, vector<int>& nums,
-           vector<bool> &mp) {
-        if(temp.size()==nums.size()){
-            ans.push_back(temp);
+    void f(int ind, vector<int>& nums, vector<vector<int>> &ans) {
+        if(ind==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(mp[i]==false){
-                temp.push_back(nums[i]);
-                mp[i]=1;
-                f(temp,ans,nums,mp);
-                temp.pop_back();
-                mp[i]=0;
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            f(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
         }
     }
 
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        vector<bool> mp(nums.size(), false);
-        f(temp, ans, nums, mp);
+        f(0,nums,ans);
         return ans;
     }
 };
